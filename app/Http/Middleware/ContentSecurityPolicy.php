0b745393213c16e26d688b805ab1a39914924290
christianldev <christlopbsc12@gmail.com>
2025-05-19 14:26:25 -0500

    feat: :ambulance: middleware para agregar el encabezado CSP a todas las respuestas

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set(
+            'Content-Security-Policy',
+            "default-src 'self'; style-src 'self' 'unsafe-inline' *.googleapis.com *.bootstrapcdn.com;"
+        );
+
+        return $response;
+    }
+}
