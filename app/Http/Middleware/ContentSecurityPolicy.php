426be385e60fbc0e9948b90b22dbe89cddc4045a
christianldev <christlopbsc12@gmail.com>
2025-05-19 14:54:08 -0500

    feat: :bug: solo quiero usar https en el proyecto

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -19,14 +19,13 @@ class ContentSecurityPolicy
         $response->headers->set(
             'Content-Security-Policy',
             "default-src 'self'; " .
-                "img-src 'self' data: https://drivegye-production.up.railway.app" .
+                "img-src 'self' data: https://drivegye-production.up.railway.app; " .
                 "style-src 'self' 'unsafe-inline' *.googleapis.com *.bootstrapcdn.com; " .
                 "script-src 'self' 'unsafe-inline' *.googleapis.com *.bootstrapcdn.com maps.googleapis.com maps.gstatic.com; " .
                 "font-src 'self' *.googleapis.com *.gstatic.com *.bootstrapcdn.com; " .
                 "connect-src 'self' maps.googleapis.com maps.gstatic.com https://maps.googleapis.com; " .
                 "frame-ancestors 'self';"
         );
-
 
         return $response;
     }
